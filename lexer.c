d1861b5858bfdd12d843190e337ca6c4c35e1078
James Plimmer <sc21jpwp@leeds.ac.uk>
2023-02-18 16:29:52 +0000

    makefile and basic function implementations, inc. EOF checker

--- lexer.c
+++ lexer.c
@@ -21,10 +21,48 @@ Date Work Commenced: 17/2/2023
 #include <ctype.h>
 #include "lexer.h"
 
-
 // YOU CAN ADD YOUR OWN FUNCTIONS, DECLARATIONS AND VARIABLES HERE
+/* pointer to the file                          */
+FILE* sCode;
+/* file name                                    */
+char* filename;
+/* variable to hold value of next read char     */
+int nextChar;
+/* variable to hold current line number         */
+int ln = 1;
 
 
+
+/* remove leading whitespace                    */
+int rmWhitespace(){
+
+    nextChar = getc(sCode);
+    if (nextChar == '\n')
+        ln++;
+
+    /* pass through all whitespace              */
+    while (isspace(nextChar)){
+        nextChar = getc(sCode);
+        if (nextChar == '\n')
+            ln++;
+    }
+
+    /* return first non-whitespace character    */
+    return nextChar;
+}
+
+/* see if next char is EOF, creating token appropriately if so  */
+Token checkEOF(Token t){
+
+    if (nextChar == EOF){
+        t.tp = EOFile;
+        strcpy(t.lx,"End Of File");
+        t.ln = ln;
+        strcpy(t.fl, filename);
+    }
+
+    return t;
+}
 
 
 
@@ -37,42 +75,73 @@ Date Work Commenced: 17/2/2023
 // This requires opening the file and making any necessary initialisations of the lexer
 // If an error occurs, the function should return 0
 // if everything goes well the function should return 1
-int InitLexer (char* file_name)
-{
-  return 0;
+int InitLexer (char* file_name){
+
+    filename = file_name;
+    /* attempt to open JACK source file       */
+    sCode = fopen(file_name, "r");
+
+    /* ensures file correctly             */
+    if (sCode == NULL){
+        printf("Error: File failed to open.\n");
+        return 0;
+    }
+    /* successful file open                     */
+    return 1;
 }
 
 
 // Get the next token from the source file
-Token GetNextToken ()
-{
+Token GetNextToken (){
+
+    /* initialise token                       */
 	Token t;
-  t.tp = ERR;
-  return t;
+    t.tp = ERR;
+
+    /* consume all leading whitespace         */
+    nextChar = rmWhitespace();
+    /* check for EOF                          */
+    t = checkEOF(t);
+
+    return t;
 }
 
 // peek (look) at the next token in the source file without removing it from the stream
-Token PeekNextToken ()
-{
-  Token t;
-  t.tp = ERR;
-  return t;
+Token PeekNextToken (){
+
+    /* initialise token                       */
+    Token t;
+    t.tp = ERR;
+
+    /* still remove all whitespace, not needing to be replaced after the peek   */
+    nextChar = rmWhitespace();
+    /* check for EOF                          */
+    t = checkEOF(t);
+    /* replace checked character              */
+    ungetc(nextChar, sCode);
+
+    return t;
 }
 
 // clean out at end, e.g. close files, free memory, ... etc
-int StopLexer ()
-{
+int StopLexer (){
+    /* close file                           */
+    fclose(sCode);
 	return 0;
 }
 
 // do not remove the next line
 #ifndef TEST
-int main ()
-{
-	// implement your main function here
-  // NOTE: the autograder will not use your main function
-
-  
+int main(int argc, char *argv[]){
+    // implement your main function here
+    // NOTE: the autograder will not use your main function
+    InitLexer(argv[1]);
+    Token t;
+    t = PeekNextToken();
+    printf("< %s, %d, %s, %d >\n", t.fl, t.ln, t.lx, t.tp);
+    t = GetNextToken();
+    printf("< %s, %d, %s, %d >\n", t.fl, t.ln, t.lx, t.tp);
+    StopLexer();
 	return 0;
 }
 // do not remove the next line
